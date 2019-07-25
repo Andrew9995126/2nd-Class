dd97c5e222f78accc3cf1b5d581b679daf1b4562
Andrew Arther <9995126@student.toiohomai.ac.nz>
2019-07-25 13:26:56 +1200

    Task 2

---
+++ 2nd Class Dec Float Double/2nd Class Dec Float Double/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2nd_Class_Dec_Float_Double
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Please enter your name:  ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Please enter your current address:  ");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Please enter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your email address: ");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Please enter a 4 digit number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello {0}, you live at {1}, you are {2} years old, your email address is {3},",name,address,age,email);
+            Console.WriteLine("and this is your four digit number {0}",num);
+            Console.ReadLine();
+
+
+        }
+    }
+}
